c8e435ece94ecaf58b456c0181be055ac617b979
Paul Fears <paulrfears@gmail.com>
2023-08-29 17:52:30 -0500

    modernized template

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,40 @@
 import { OnRpcRequestHandler } from '@metamask/snap-types';
-
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+import { Metamask } from './Metamask';
+export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
+  const params = request.params;
   switch (request.method) {
     case 'hello':
-      return wallet.request({
-        method: 'snap_confirm',
-        params: [
-          {
-            prompt: `Hello, ${origin}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
-      });
+      return await Metamask.sendConfirmation("hello", "yes", "okay")
     default:
       throw new Error('Method not found.');
   }
 };
+/*
+const account: {
+  id: uuid(),
+  name,
+  options,
+  address,
+  supportedMethods: [
+    'eth_sendTransaction',
+    'eth_sign',
+    'eth_signTransaction',
+    'eth_signTypedData_v1',
+    'eth_signTypedData_v2',
+    'eth_signTypedData_v3',
+    'eth_signTypedData_v4',
+    'eth_signTypedData',
+    'personal_sign',
+  ],
+  type: 'eip155:eoa',
+};
+
+await snap.request({
+  method: 'snap_manageAccounts',
+  params: {
+    method: 'createAccount',
+    params: { account },
+  },
+});
+return account;
+*/

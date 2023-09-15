677110a78c176d2e8695a12f6cd23cfd5e910b83
Paul Fears <paulrfears@gmail.com>
2023-09-14 20:15:09 -0500

    updated snap id

--- site/src/constants.ts
+++ site/src/constants.ts
@@ -1,1 +1,1 @@
-export const snapId = import.meta.env.DEV? "local:http://localhost:8080/" : "stellar-snap"
+export const snapId = import.meta.env.DEV? "local:http://localhost:8080/" : "npm:stellar-snap"

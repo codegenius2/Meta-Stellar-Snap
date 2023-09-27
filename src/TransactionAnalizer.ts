7524517953444f1bbc93381c9f861bc5988f9be4
Paul Fears <paulrfears@gmail.com>
2023-09-26 20:00:58 -0500

    updated stellar-base version

--- src/TransactionAnalizer.ts
+++ src/TransactionAnalizer.ts
@@ -6,7 +6,39 @@ import { TransactionBuilder, Transaction, FeeBumpTransaction, xdr} from "stellar
 import { Client } from "./Client";
 import { isSorobanTransaction, assembleTransaction } from "./sorobanTxn";
 import { TxnBuilder } from "./TxnBuilder";
-
+/**
+ * 
+ * Operation.createAccount
+Operation.payment
+Operation.pathPaymentStrictReceive
+Operation.pathPaymentStrictSend
+Operation.manageSellOffer
+Operation.manageBuyOffer
+Operation.createPassiveSellOffer
+Operation.setOptions
+Operation.changeTrust
+Operation.allowTrust
+Operation.accountMerge
+Operation.inflation
+Operation.manageData
+Operation.bumpSequence
+Operation.createClaimableBalance
+Operation.claimClaimableBalance
+Operation.beginSponsoringFutureReserves
+Operation.endSponsoringFutureReserves
+Operation.revokeAccountSponsorship
+Operation.revokeTrustlineSponsorship
+Operation.revokeOfferSponsorship
+Operation.revokeDataSponsorship
+Operation.revokeClaimableBalanceSponsorship
+Operation.revokeLiquidityPoolSponsorship
+Operation.revokeSignerSponsorship
+Operation.clawback
+Operation.clawbackClaimableBalance
+Operation.setTrustLineFlags
+Operation.liquidityPoolDeposit
+Operation.liquidityPoolWithdraw
+ */
 export class TransactionAnalizer{
     client;
     constructor(client: Client){
@@ -18,6 +50,96 @@ export class TransactionAnalizer{
         const uiList = [];
         if(operation.type === 'payment'){
             uiList.push(text('payment'))
+        }
+        else if(operation.type === 'createAccount'){
+            uiList.push(text('createAccount'))
+        }
+        else if(operation.type === 'pathPaymentStrictReceive'){
+
+        }
+        else if(operation.type === 'pathPaymentStrictSend'){
+
+        }
+        else if(operation.type === 'manageSellOffer'){
+
+        }
+        else if(operation.type === 'createPassiveSellOffer'){
+
+        }
+        else if(operation.type === 'setOptions'){
+
+        }
+        else if(operation.type === 'changeTrust'){
+
+        }
+        else if(operation.type === 'allowTrust'){
+
+        }
+        else if(operation.type === 'accountMerge'){
+
+        }
+        else if(operation.type === 'inflation'){
+
+        }
+        else if(operation.type === 'manageData'){
+
+        }
+        else if(operation.type === 'bumpSequence'){
+
+        }
+        else if(operation.type === 'createClaimableBalance'){
+
+        }
+        else if(operation.type === 'claimClaimableBalance'){
+
+        }
+        else if(operation.type === 'beginSponsoringFutureReserves'){
+
+        }
+        else if(operation.type === 'endSponsoringFutureReserves'){
+
+        }
+        else if(operation.type === 'revokeAccountSponsorship'){
+
+        }
+        else if(operation.type === 'revokeTrustlineSponsorship'){
+
+        }
+        else if(operation.type === 'revokeOfferSponsorship'){
+
+        }
+        else if(operation.type === 'revokeDataSponsorship'){
+
+        }
+        else if(operation.type === 'revokeClaimableBalanceSponsorship'){
+
+        }
+        else if(operation.type === 'revokeLiquidityPoolSponsorship'){
+
+        }
+        else if(operation.type === 'revokeSignerSponsorship'){
+
+        }
+        else if(operation.type === 'clawback'){
+
+        }
+        else if(operation.type === 'clawbackClaimableBalance'){
+
+        }
+        else if(operation.type === 'setTrustLineFlags'){
+
+        }
+        else if(operation.type === 'liquidityPoolDeposit'){
+
+        }
+        else if(operation.type === 'liquidityPoolWithdraw'){
+
+        }
+        else if(operation.type === 'invokeHostFunction'){
+
+        }
+        else if(operation.type === ''){
+
         }
         return {uiList, currentValue}
     }
